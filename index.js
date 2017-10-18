1379f038fb0fe653e66a9a2ae968d3f467f1d70d
Alex <alex@monney.me>
2017-10-18 21:59:05 +0100

    refactor and add option to embed with no div wrapper and fewer html params

--- index.js
+++ index.js
@@ -6,39 +6,66 @@
 // Process @[prezi](preziID)
 
 (function() {
-
   var self = this; // root will be window object in browser context
 
-  var yt_regex = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
-  function youtube_parser (url) {
-    var match = url.match(yt_regex);
-    return match && match[7].length === 11 ? match[7] : url;
-  }
-
   /* eslint-disable max-len */
-  var vimeo_regex = /https?:\/\/(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|)(\d+)(?:$|\/|\?)/;
+  /* eslint-disable no-unused-vars */
+  var services = {
+    youtube : {
+      regex : /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/,
+      parser : function(url) {
+        var match = url.match(services.youtube.regex);
+        return match && match[7].length === 11 ? match[7] : url;
+      },
+      url : function(videoID, options) {
+        return '//www.youtube.com/embed/' + videoID;
+      },
+      options: { width: 640, height: 390 }
+    },
+    vimeo : {
+      regex : /https?:\/\/(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|)(\d+)(?:$|\/|\?)/,
+      parser : function(url) {
+        var match = url.match(services.vimeo.regex);
+        return match && typeof match[3] === 'string' ? match[3] : url;
+      },
+      url : function(videoID, options) {
+        return '//player.vimeo.com/video/' + videoID;
+      },
+      options: { width: 500, height: 281 }
+    },
+    vine : {
+      regex : /^http(?:s?):\/\/(?:www\.)?vine\.co\/v\/([a-zA-Z0-9]{1,13}).*/,
+      parser : function(url) {
+        var match = url.match(services.vine.regex);
+        return match && match[1].length === 11 ? match[1] : url;
+      },
+      url : function(videoID, options) {
+        return '//vine.co/v/' + videoID + '/embed/' + services.vine.options.embed;
+      },
+      options: { width: 600, height: 600, embed: 'simple' }
+    },
+    prezi : {
+      regex : /^https:\/\/prezi.com\/(.[^/]+)/,
+      parser : function(url) {
+        var match = url.match(services.prezi.regex);
+        return match ? match[1] : url;
+      },
+      url : function(videoID, options) {
+        return 'https://prezi.com/embed/' + videoID +
+        '/?bgcolor=ffffff&amp;lock_to_path=0&amp;autoplay=0&amp;autohide_ctrls=0&amp;' +
+        'landing_data=' +
+          'bHVZZmNaNDBIWnNjdEVENDRhZDFNZGNIUE43MHdLNWpsdFJLb2ZHanI5N1lQVHkxSHFxazZ0UUNCRHloSXZROHh3PT0&amp;' +
+        'landing_sign=1kD6c0N6aYpMUS0wxnQjxzSqZlEB8qNFdxtdjYhwSuI';
+      },
+      options: { width: 550, height: 400 }
+    }
+  };
   /* eslint-enable max-len */
-  function vimeo_parser (url) {
-    var match = url.match(vimeo_regex);
-    return match && typeof match[3] === 'string' ? match[3] : url;
-  }
+  /* eslint-enable no-unused-vars */
 
-  var vine_regex = /^http(?:s?):\/\/(?:www\.)?vine\.co\/v\/([a-zA-Z0-9]{1,13}).*/;
-  function vine_parser (url) {
-    var match = url.match(vine_regex);
-    return match && match[1].length === 11 ? match[1] : url;
-  }
 
-  var prezi_regex = /^https:\/\/prezi.com\/(.[^/]+)/;
-  function prezi_parser(url) {
-    var match = url.match(prezi_regex);
-    return match ? match[1] : url;
-  }
-
-  var EMBED_REGEX = /@\[([a-zA-Z].+)\]\([\s]*(.*?)[\s]*[\)]/im;
-
-  function video_embed(md, options) {
-    function video_return(state, silent) {
+  function video_embed(md) {
+    return function(state, silent) {
       var serviceEnd,
         serviceStart,
         token,
@@ -49,7 +76,8 @@
         return false;
       }
 
-      var match = EMBED_REGEX.exec(state.src);
+      var EMBED = /@\[([a-zA-Z].+)\]\([\s]*(.*?)[\s]*[\)]/im;
+      var match = EMBED.exec(state.src);
 
       if (!match || match.length < 3) {
         return false;
@@ -59,15 +87,9 @@
       var videoID = match[2];
       var serviceLower = service.toLowerCase();
 
-      if (serviceLower === 'youtube') {
-        videoID = youtube_parser(videoID);
-      } else if (serviceLower === 'vimeo') {
-        videoID = vimeo_parser(videoID);
-      } else if (serviceLower === 'vine') {
-        videoID = vine_parser(videoID);
-      } else if (serviceLower === 'prezi') {
-        videoID = prezi_parser(videoID);
-      } else if (!options[serviceLower]) {
+      if (services[serviceLower]) {
+        videoID = services[serviceLower].parser(videoID);
+      } else {
         return false;
       }
 
@@ -99,64 +121,52 @@
       state.pos = state.pos + state.src.indexOf(')', state.pos);
       state.posMax = state.tokens.length;
       return true;
-    }
-
-    return video_return;
+    };
   }
 
-  function video_url(service, videoID, options) {
-    switch (service) {
-      case 'youtube':
-        return '//www.youtube.com/embed/' + videoID;
-      case 'vimeo':
-        return '//player.vimeo.com/video/' + videoID;
-      case 'vine':
-        return '//vine.co/v/' + videoID + '/embed/' + options.vine.embed;
-      case 'prezi':
-        return 'https://prezi.com/embed/' + videoID +
-        '/?bgcolor=ffffff&amp;lock_to_path=0&amp;autoplay=0&amp;autohide_ctrls=0&amp;' +
-        'landing_data=' +
-          'bHVZZmNaNDBIWnNjdEVENDRhZDFNZGNIUE43MHdLNWpsdFJLb2ZHanI5N1lQVHkxSHFxazZ0UUNCRHloSXZROHh3PT0&amp;' +
-        'landing_sign=1kD6c0N6aYpMUS0wxnQjxzSqZlEB8qNFdxtdjYhwSuI';
-    }
-    return '';
+  function iframeparams(options, service) {
+    if (options.plainiframe) {return '';}
+    return ' class="embed-responsive-item"'
+      + ' id="' + service + 'player"'
+      + ' type="text/html"'
+      + ' width="' + options[service].options.width + '"'
+      + ' height="' + options[service].options.height + '"';
   }
 
   function tokenize_video(md, options) {
-    function tokenize_return(tokens, idx) {
+    return function(tokens, idx) {
       var videoID = md.utils.escapeHtml(tokens[idx].videoID);
       var service = md.utils.escapeHtml(tokens[idx].service).toLowerCase();
-      return videoID === '' ? '' :
-        '<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" id="' +
-        service + 'player" type="text/html" width="' + options[service].width +
-        '" height="' + options[service].height +
-        '" src="' + options.url(service, videoID, options) +
-        '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>';
-    }
+      if (videoID === '') { return ''; }
 
-    return tokenize_return;
+
+      var html = '<iframe' + iframeparams(options, service) + ' src="' + options[service].url(videoID, options) + '"';
+      html += ' frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+
+      return options.plainiframe === true
+        ? html : '<div class="embed-responsive embed-responsive-16by9">' + html + '</div>';
+    };
   }
 
-  var defaults = {
-    url: video_url,
-    youtube: { width: 640, height: 390 },
-    vimeo: { width: 500, height: 281 },
-    vine: { width: 600, height: 600, embed: 'simple' },
-    prezi: { width: 550, height: 400 }
-  };
-
   function video_plugin(md, options) {
+    console.log(options);
     if (options) {
-      Object.keys(defaults).forEach(function(key) {
-        if (typeof options[key] === 'undefined') {
-          options[key] = defaults[key];
+      Object.keys(services).forEach(function(service) {
+        if (typeof options[service] === 'undefined') {
+          options[service] = services[service];
+        } else {
+          Object.keys(services[service]).forEach(function(property) {
+            if (typeof options[service][property] === 'undefined') {
+              options[service][property] = services[service][property];
+            }
+          });
         }
       });
     } else {
-      options = defaults;
+      options = services;
     }
     md.renderer.rules.video = tokenize_video(md, options);
-    md.inline.ruler.before('emphasis', 'video', video_embed(md, options));
+    md.inline.ruler.before('emphasis', 'video', video_embed(md));
   }
 
   if (typeof exports !== 'undefined') {
--- test/test.js
+++ test/test.js
@@ -11,3 +11,14 @@ describe('markdown-it-video', function() {
   }).use(require('../'));
   generate(path.join(__dirname, 'fixtures/video.txt'), md);
 });
+
+describe('markdown-it-video options', function() {
+  var md = require('markdown-it')({
+    html: true,
+    linkify: true,
+    typography: true
+  }).use(require('../index.js'), {
+    'plainiframe' : true
+  });
+  generate(path.join(__dirname, 'fixtures/video_options.txt'), md);
+});
